64507e528ef3162701c4e2c0915015689b1cff2f
Robson Vaamonde <robsonvaamonde@gmail.com>
2016-10-30 13:19:37 -0200

    Create 00-kali.sh

---
+++ 01_day/00-kali.sh
@@ -0,0 +1,17 @@
+#Distribuições GNU/Linux utilizadas para Pen Test.
+
+Backtrack - http://www.backtrack-linux.org/
+NodeZero - http://nodezero-linux.org/
+Caine - http://www.caine-live.net/
+Deft - http://www.deftlinux.net/
+FDTK - http://fdtk.com.br/www/
+Matriux - http://www.matriux.com/
+Live Hacking DVD - http://www.livehacking.com/
+Backbox Linux - http://www.backbox.org/
+Samurai WTF - http://samurai.inguardians.com/
+Knoppix STD - http://s-t-d.org/
+Pentoo - http://www.pentoo.ch/
+WEAKERTH4N Linux - http://weaknetlabs.com/main/?page_id=479
+
+#Solução que será utilizada na #GeekWeekSenac
+Kali - https://www.kali.org/
