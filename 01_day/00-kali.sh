e3eee148e18c7ae97b02e83f3a015e1668592bf7
Robson Vaamonde <robsonvaamonde@gmail.com>
2016-10-31 08:14:45 -0200

    Update 00-kali.sh

--- 01_day/00-kali.sh
+++ 01_day/00-kali.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+# Autor: Robson Vaamonde
+# Site: www.procedimentosemti.com.br
+# Facebook: facebook.com/ProcedimentosEmTI
+# Facebook: facebook.com/BoraParaPratica
+# YouTube: youtube.com/BoraParaPratica
+# Data de criação: 31/10/2016
+# Data de atualização: 31/10/2016
+# Versão: 1.0
+
 #Distribuições GNU/Linux utilizadas para Pen Test.
 
 Backtrack - http://www.backtrack-linux.org/
