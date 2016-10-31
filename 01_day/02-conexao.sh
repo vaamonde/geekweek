66896e2f36c55296edb1c1ae1ce98d8e0e1643d4
Robson Vaamonde <robsonvaamonde@gmail.com>
2016-10-31 09:57:02 -0200

    Update 02-conexao.sh

--- 01_day/02-conexao.sh
+++ 01_day/02-conexao.sh
@@ -14,10 +14,12 @@
 #Comandos básicos de teste de conexão com a internet do GNU/Linux Kali
 
 #Pingando o Google, o comando ping faz o teste fim-a-fim, traduzindo Nome para IP.
+#64 bytes = tamanho do pacote, TTL = Time to Live, Time= Tempo de Resposta
 ping www.google.com
 
 #Descobrindo o endereço IP do domínio google.com
 host www.google.com
+host -t CNAME
 
 #Verificando os servidores de DNS responsável pela tradução de nomes do domínio google.com
 nslookup www.google.com
@@ -33,3 +35,19 @@ whois www.google.com
 
 #Verificando as rotas (saltos) até o domínio google.com
 traceroute www.google.com
+
+#Verificando as conexões estabelecidas
+#n=não traduzir nomes, a=tudo, t=tcp, u=udp, p=processo
+netstat -natup
+
+#Verificando informações de trafégo
+netstat -i
+
+#Acesando serviços com o Telnet
+telnet ALVO 80
+
+#Analisando os pacotes enviados pela Interface com o TCPdump
+tcpdump
+
+#Vendo a tabela de ARP Local
+arp -a
