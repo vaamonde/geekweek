3d8ed9d4fed091a80f8d4ed24331f3dd43f5b4fb
Robson Vaamonde <robsonvaamonde@gmail.com>
2016-11-02 13:25:14 -0200

    Create 10-su.sh

---
+++ 02_day/10-su.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Autor: Robson Vaamonde
+# Site: www.procedimentosemti.com.br
+# Facebook: facebook.com/ProcedimentosEmTI
+# Facebook: facebook.com/BoraParaPratica
+# YouTube: youtube.com/BoraParaPratica
+# Data de criação: 31/10/2016
+# Data de atualização: 31/10/2016
+# Versão: 1.0
+
+# Descompactando o arquivo .deb do Sucrack
+ar -x sucrack.deb >> /dev/null
+
+# Descompactando o arquivo data.tar.gz do Sucrack
+tar -zxvf data.tar.gz >> /dev/null
+
+#Acessando o diretório do seu binário
+cd usr/bin
+
+#Executando o binário do Sucrack utilizando a Wordlist senhas.txt
+./sucrack ../../senhas.txt
