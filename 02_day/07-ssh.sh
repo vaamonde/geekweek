7e464e6091ffaa3076bae73846b6353cde07e3b4
Robson Vaamonde <robsonvaamonde@gmail.com>
2016-11-02 12:57:37 -0200

    Create 07-ssh.sh

---
+++ 02_day/07-ssh.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Autor: Robson Vaamonde
+# Site: www.procedimentosemti.com.br
+# Facebook: facebook.com/ProcedimentosEmTI
+# Facebook: facebook.com/BoraParaPratica
+# YouTube: youtube.com/BoraParaPratica
+# Data de criação: 31/10/2016
+# Data de atualização: 31/10/2016
+# Versão: 1.0
+
+#Variável do comando Hydra
+ALVO="www.SEU_ALVO.br"
