094d408bcf3d59dd1a924af90361054a45c97703
Robson Vaamonde <robsonvaamonde@gmail.com>
2016-11-04 13:51:32 -0200

    Create FinalizeSuaMissao.sh

---
+++ 03_day/FinalizeSuaMissao.sh
@@ -0,0 +1,11 @@
+#Finalize sua Missão!!!!!
+
+Endereço para sua missão: http://geekweek-vaamonde.rhcloud.com/
+
+
+#Objetivo:
+Acessar o WP-Admin do Wordpress, fazer uma pichação com o seu Nome, Data, Hora.
+
+#BoaSortePessoal!!!!!
+
+Robson Vaamonde
